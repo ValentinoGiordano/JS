6e1a788e70f155df79f6b2372e4aa6943b39174d
Valentino Giordano <valentinogiordanotorres22@gmail.com>
2022-07-21 21:38:26 -0300

    simulador entregable

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-let Numero = parseInt (prompt("Write a number"));
+/*let Numero = parseInt (prompt("Write a number"));
 for (let i = 1; i <=5; i++){
     let resultado = Numero / i ;
     alert(Numero +" / "+ i +" = "+ resultado);
@@ -12,6 +12,29 @@ while(datos != "ESC"){
     age = prompt("How old are you");
     alert(" The user is "+ age + " years old ");
     break
+}*/
+let saldo = parseFloat(prompt("Tu saldo es:"));
+let prenda = "";
+let gastos = 0;
+
+while (prenda.toUpperCase() != "ESC") {
+    prenda = prompt("Ingrese el nombre de la prenda:");
+
+    if (prenda.toUpperCase() == "ESC") {
+        alert("Tu saldo es de: $" + gastos + " pesos ");
+        break;
+    }
+
+    console.log("Nombre: " + prenda);
+    let precio = parseFloat(prompt("Ingrese el precio de su prenda:"));
+    console.log("Precio: " + precio);
+    gastos += precio;
+
+    if (gastos > saldo){
+        alert("Superaste el límite de tu saldo");
+        alert("El saldo es: $" + gastos + " pesos ");
+        break;
+    }
 }
 
 
