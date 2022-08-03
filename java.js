8d30b1743ed70f2bb2e4213523694511bbc7e014
Valentino Giordano <valentinogiordanotorres22@gmail.com>
2022-08-02 23:18:49 -0300

    proyecto final 1

--- java.js
+++ java.js
@@ -1,3 +1,38 @@
+class info{
+    constructor(){
+    this.nombre=prompt("Ingrese su nombre"); 
+    this.edad=prompt("Ingrese su edad"); 
+    this.telefono=prompt("Ingrese su telefono"); 
+    this.email=prompt("Ingrese su mail");  
+}
+}
+
+const datos = new info();
+
+
+alert(`Nombre: ${datos.nombre} 
+        Edad: ${datos.edad} 
+        Telefono: ${datos.telefono} 
+        Email: ${datos.email} 
+`)
+
+let resultado = 0
+
+function sumar(valor1, valor2, valor3) {
+    resultado = (valor1 + valor2 + valor3);
+}
+
+function mostrarMensaje(mensaje) {
+    alert("El resultado es " + mensaje);
+}
+
+let var1 = parseInt(prompt("Ingrese un valor"));
+let var2 = parseInt(prompt("Ingrese un valor"));
+let var3 = parseInt(prompt("Ingrese un valor"));
+
+sumar(var1, var2, var3);
+mostrarMensaje(resultado); 
+
 function pais() {
     alert("A continuación te recomendamos algunos países para vacacionar 😎");
 }
@@ -17,4 +52,5 @@ paises.sort((a, b) => {
     return a.id - b.id;
 })
 
-agregar();
+agregar(); 
+
